79d36c6fc022777b543f02df829bdf8f5d26cb26
Nukdata <65764317+Nukdata@users.noreply.github.com>
2020-06-07 14:35:28 +0800

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,33 +1,41 @@
-X_train <- read.table("./data/train/X_train.txt")               ## read sthe training set
-y_train <- read.table("./data/train/y_train.txt")               ## reads the training labels
-subject_train <- read.table("./data/train/subject_train.txt")   ## reads the subject who performed the activity [1-30]
+library(dplyr)
 
+# read train data
+X_train <- read.table("./UCI HAR Dataset/train/X_train.txt")
+Y_train <- read.table("./UCI HAR Dataset/train/Y_train.txt")
+Sub_train <- read.table("./UCI HAR Dataset/train/subject_train.txt")
 
-X_test <- read.table("./data/test/X_test.txt")              ## readsthe test set
-y_test <- read.table("./data/test/y_test.txt")              ## reads the test labels    
-subject_test <- read.table("./data/test/subject_test.txt")  
+# read test data
+X_test <- read.table("./UCI HAR Dataset/test/X_test.txt")
+Y_test <- read.table("./UCI HAR Dataset/test/Y_test.txt")
+Sub_test <- read.table("./UCI HAR Dataset/test/subject_test.txt")
 
+# read data description
+variable_names <- read.table("./UCI HAR Dataset/features.txt")
 
-features <- read.table("./data/features.txt")   ## reads the list of all features
-mergedSets <- rbind(X_train, X_test) 
-mergedSets$labels <- rbind(y_train, y_test) 
-mergedSets$subjects <- rbind(subject_train, subject_test) 
-colnames(mergedSets) <- features$V2   ## gives cols the names
+# read activity labels
+activity_labels <- read.table("./UCI HAR Dataset/activity_labels.txt")
 
+# 1. Merges the training and the test sets to create one data set.
+X_total <- rbind(X_train, X_test)
+Y_total <- rbind(Y_train, Y_test)
+Sub_total <- rbind(Sub_train, Sub_test)
 
-df2 <- mergedSets[,grepl("mean()|std()", names(mergedSets))]
-df2 <- cbind(subjects = rbind(subject_train, subject_test), labels = rbind(y_train, y_test), df2)
-colnames(df2)[1]<-"subject"
-colnames(df2)[2]<-"activity"
-activity_names <- read.table("./data/activity_labels.txt")   ## reads the class labels with their activity name
+# 2. Extracts only the measurements on the mean and standard deviation for each measurement.
+selected_var <- variable_names[grep("mean\\(\\)|std\\(\\)",variable_names[,2]),]
+X_total <- X_total[,selected_var[,1]]
 
+# 3. Uses descriptive activity names to name the activities in the data set
+colnames(Y_total) <- "activity"
+Y_total$activitylabel <- factor(Y_total$activity, labels = as.character(activity_labels[,2]))
+activitylabel <- Y_total[,-1]
 
-library(qdapTools)
-df2[,2] <- lookup(df2[,2], activity_names, key.reassign = NULL,
-       missing = NA)   ## looks up for activities according to given num
+# 4. Appropriately labels the data set with descriptive variable names.
+colnames(X_total) <- variable_names[selected_var[,1],2]
 
-## print(str(df2))
-
-## library("writexl")      
-## write_xlsx(df2,"./data/tidy-data.xlsx")    ## writes the new data into xlsx format
-write.table(df2,"./data/tidy-data.txt", row.name=FALSE) ## writes the new data into txt format
+# 5. From the data set in step 4, creates a second, independent tidy data set with the average
+# of each variable for each activity and each subject.
+colnames(Sub_total) <- "subject"
+total <- cbind(X_total, activitylabel, Sub_total)
+total_mean <- total %>% group_by(activitylabel, subject) %>% summarize_each(funs(mean))
+write.table(total_mean, file = "./UCI HAR Dataset/tidydata.txt", row.names = FALSE, col.names = TRUE)
